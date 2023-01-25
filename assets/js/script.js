5e41dca3fbd4fbd79f9c5dd466404e4253f0b523
Dali Han <ross4han@gmail.com>
2023-01-25 09:09:33 +0000

    api call examples

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// api docs // how to call API
+
+// weather : 
+// var xhr = $.get("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=1&appid=");
+// xhr.done(function(data) { console.log("success got data", data); });
+
+// geoname decode
+// var xhr = $.get("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=");
+// xhr.done(function(data) { console.log("success got data", data); });
