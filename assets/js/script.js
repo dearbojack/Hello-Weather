7140cac916c0c6fa398219df95d46e2bd6d71523
Dali Han <ross4han@gmail.com>
2023-01-25 22:00:21 +0000

    testing dummy html parts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
 // api docs // how to call API
 
 // weather : 
-// var xhr = $.get("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=1&appid=");
+// https://api.openweathermap.org/data/2.5/weather?q=London&appid={API key}
+// geoname decode
+// var xhr = $.get("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&limit=5&appid=95d15e0dac6e9067bba1f640b9fb69f0");
 // xhr.done(function(data) { console.log("success got data", data); });
 
-// geoname decode
-// var xhr = $.get("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=");
-// xhr.done(function(data) { console.log("success got data", data); });
+// ! how to get icon
+// How to get icon URL
+// For code 500 - light rain icon = "10d". See below a full list of codes
+// URL is http://openweathermap.org/img/wn/10d@2x.png
